8a32a9036b95ce922c1cb2df81b2441e0b9767e0
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-12 21:50:29 -0500

    Removing the if condition in the height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,26 +1,4 @@
 #include "binary_trees.h"
-/**
- * binary_tree_height - measure the height of a binary tree.
- * @tree: tree given.
- *
- * Return: the height of the tree.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	unsigned int left_side, right_side;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left != NULL || tree->right != NULL)
-	{
-		left_side = binary_tree_height(tree->left);
-		right_side = binary_tree_height(tree->right);
-		if (right_side > left_side)
-			return (right_side + 1);
-		return (left_side + 1);
-	}
-	return (0);
-}
 
 /**
  * binary_tree_balance - function that measure the balance
@@ -32,12 +10,32 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left, right;
+	int left = 0, right = 0;
 
 	if (tree == NULL)
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
-
 	return (left - right);
+}
+
+
+/**
+ * binary_tree_height - measure the height of a binary tree.
+ * @tree: tree given.
+ *
+ * Return: the height of the tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	unsigned int left_side = 0, right_side = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_side = binary_tree_height(tree->left);
+	right_side = binary_tree_height(tree->right);
+	if (right_side > left_side)
+		return (right_side + 1);
+	return (left_side + 1);
 }
